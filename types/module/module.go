35531769f5604317f50821531ca6ea6e2c971bb5
Andrej Zavgorodnij <a.zavgorodny@corp.mail.ru>
2020-02-24 21:37:06 +0300

    feat: abci extension

--- types/module/module.go
+++ types/module/module.go
@@ -30,6 +30,7 @@ package module
 
 import (
 	"encoding/json"
+
 	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 
@@ -144,7 +145,7 @@ type AppModule interface {
 
 	// ABCI
 	BeginBlock(sdk.Context, abci.RequestBeginBlock)
-	EndBlock(sdk.Context, abci.RequestEndBlock) []abci.ValidatorUpdate
+	EndBlock(sdk.Context, abci.RequestEndBlock) abci.ModuleEndBlock
 }
 
 //___________________________
@@ -180,8 +181,8 @@ func (gam GenesisOnlyAppModule) NewQuerierHandler() sdk.Querier { return nil }
 func (gam GenesisOnlyAppModule) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) {}
 
 // EndBlock returns an empty module end-block
-func (GenesisOnlyAppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	return []abci.ValidatorUpdate{}
+func (GenesisOnlyAppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) abci.ModuleEndBlock {
+	return abci.ModuleEndBlock{}
 }
 
 //____________________________________________________________________________
@@ -306,10 +307,12 @@ func (m *Manager) BeginBlock(ctx sdk.Context, req abci.RequestBeginBlock) abci.R
 // modules.
 func (m *Manager) EndBlock(ctx sdk.Context, req abci.RequestEndBlock) abci.ResponseEndBlock {
 	ctx = ctx.WithEventManager(sdk.NewEventManager())
-	validatorUpdates := []abci.ValidatorUpdate{}
+	var validatorUpdates []abci.ValidatorUpdate
+	var seed []byte
 
 	for _, moduleName := range m.OrderEndBlockers {
-		moduleValUpdates := m.Modules[moduleName].EndBlock(ctx, req)
+		moduleEndBlock := m.Modules[moduleName].EndBlock(ctx, req)
+		moduleValUpdates := moduleEndBlock.Validators
 
 		// use these validator updates if provided, the module manager assumes
 		// only one module will update the validator set
@@ -320,6 +323,15 @@ func (m *Manager) EndBlock(ctx sdk.Context, req abci.RequestEndBlock) abci.Respo
 
 			validatorUpdates = moduleValUpdates
 		}
+
+		if len(moduleEndBlock.Seed) > 0 {
+			if len(seed) > 0 {
+				panic("seed EndBlock updates already set by a previous module")
+			}
+
+			seed = moduleEndBlock.Seed
+		}
+
 	}
 
 	return abci.ResponseEndBlock{
